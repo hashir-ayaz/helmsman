9b450360b6c5e4c152c5708ad6e4b3d986cfd7f6
Hashir Ayaz <hashirayaz@gmail.com>
2026-06-12 18:22:36 +0500

    feat: add k8s.Watch() goroutine-backed channel for resource watch events

---
+++ helmsman-api/internal/k8s/watch.go
@@ -0,0 +1,93 @@
+package k8s
+
+import (
+	"context"
+	"net/http"
+	"time"
+
+	"github.com/hashir-ayaz/helmsman/helmsman-api/internal/cluster"
+
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
+	watchpkg "k8s.io/apimachinery/pkg/watch"
+)
+
+// WatchEvent is the payload streamed to the frontend for each K8s watch event.
+type WatchEvent struct {
+	Type      string `json:"type"`
+	Name      string `json:"name"`
+	Namespace string `json:"namespace,omitempty"`
+}
+
+// Watch opens a K8s watch on the given resource and writes WatchEvent values to
+// the returned channel. It reconnects automatically when the API server closes
+// the watch. The channel is closed when ctx is cancelled.
+func Watch(ctx context.Context, b *cluster.ClientBundle, ref ResourceRef, namespace string) (<-chan WatchEvent, error) {
+	ch := make(chan WatchEvent, 64)
+	go func() {
+		defer close(ch)
+		var resourceVersion string
+		for {
+			watcher, err := dynResource(b.Dynamic, ref, namespace).Watch(ctx, metav1.ListOptions{
+				ResourceVersion: resourceVersion,
+			})
+			if err != nil {
+				// Reconnect after back-off; avoids spinning on persistent errors.
+				if !backoff(ctx) {
+					return
+				}
+				continue
+			}
+		loop:
+			for {
+				select {
+				case <-ctx.Done():
+					watcher.Stop()
+					return
+				case event, ok := <-watcher.ResultChan():
+					if !ok {
+						break loop // server closed watch; reconnect
+					}
+					if event.Type == watchpkg.Error {
+						// Only reset resourceVersion on 410 Gone.
+						if status, ok := event.Object.(*metav1.Status); ok && status.Code == http.StatusGone {
+							resourceVersion = ""
+						}
+						break loop
+					}
+					obj, ok := event.Object.(*unstructured.Unstructured)
+					if !ok {
+						continue
+					}
+					resourceVersion = obj.GetResourceVersion()
+					we := WatchEvent{
+						Type:      string(event.Type),
+						Name:      obj.GetName(),
+						Namespace: obj.GetNamespace(),
+					}
+					select {
+					case ch <- we:
+					case <-ctx.Done():
+						watcher.Stop()
+						return
+					}
+				}
+			}
+			watcher.Stop()
+			if !backoff(ctx) {
+				return
+			}
+		}
+	}()
+	return ch, nil
+}
+
+// backoff pauses 2 seconds or until ctx is cancelled; returns false if ctx fired.
+func backoff(ctx context.Context) bool {
+	select {
+	case <-time.After(2 * time.Second):
+		return true
+	case <-ctx.Done():
+		return false
+	}
+}
---
+++ helmsman-api/internal/k8s/watch_test.go
@@ -0,0 +1,125 @@
+package k8s
+
+import (
+	"context"
+	"testing"
+	"time"
+
+	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
+	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/apimachinery/pkg/runtime/schema"
+	watchpkg "k8s.io/apimachinery/pkg/watch"
+	dynamicfake "k8s.io/client-go/dynamic/fake"
+	k8stesting "k8s.io/client-go/testing"
+
+	"github.com/hashir-ayaz/helmsman/helmsman-api/internal/cluster"
+)
+
+func newFakePod(name, ns, rv string) *unstructured.Unstructured {
+	return &unstructured.Unstructured{Object: map[string]any{
+		"apiVersion": "v1", "kind": "Pod",
+		"metadata": map[string]any{"name": name, "namespace": ns, "resourceVersion": rv},
+	}}
+}
+
+func fakeWatchSetup() (*dynamicfake.FakeDynamicClient, *watchpkg.FakeWatcher) {
+	dynClient := dynamicfake.NewSimpleDynamicClient(runtime.NewScheme())
+	fw := watchpkg.NewFake()
+	dynClient.PrependWatchReactor("*", func(_ k8stesting.Action) (bool, watchpkg.Interface, error) {
+		return true, fw, nil
+	})
+	return dynClient, fw
+}
+
+func podRef() ResourceRef {
+	return ResourceRef{
+		GVR:        schema.GroupVersionResource{Version: "v1", Resource: "pods"},
+		Namespaced: true,
+	}
+}
+
+func TestWatch_deliversModifiedEvent(t *testing.T) {
+	dynClient, fw := fakeWatchSetup()
+	ctx, cancel := context.WithTimeout(context.Background(), 2*time.Second)
+	defer cancel()
+
+	b := &cluster.ClientBundle{Dynamic: dynClient}
+	ch, err := Watch(ctx, b, podRef(), "default")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	go func() {
+		time.Sleep(10 * time.Millisecond)
+		fw.Modify(newFakePod("my-pod", "default", "100"))
+	}()
+
+	select {
+	case event := <-ch:
+		if event.Type != "MODIFIED" {
+			t.Errorf("want MODIFIED, got %s", event.Type)
+		}
+		if event.Name != "my-pod" {
+			t.Errorf("want my-pod, got %s", event.Name)
+		}
+		if event.Namespace != "default" {
+			t.Errorf("want default, got %s", event.Namespace)
+		}
+	case <-ctx.Done():
+		t.Fatal("timed out waiting for event")
+	}
+}
+
+func TestWatch_closesChannelOnContextCancel(t *testing.T) {
+	dynClient, _ := fakeWatchSetup()
+	ctx, cancel := context.WithCancel(context.Background())
+	b := &cluster.ClientBundle{Dynamic: dynClient}
+
+	ch, err := Watch(ctx, b, podRef(), "default")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	cancel()
+
+	select {
+	case _, open := <-ch:
+		if open {
+			t.Error("channel should be closed after context cancel")
+		}
+	case <-time.After(3 * time.Second):
+		t.Fatal("channel not closed within 3 seconds of cancel")
+	}
+}
+
+func TestWatch_deliversAddedAndDeletedEvents(t *testing.T) {
+	dynClient, fw := fakeWatchSetup()
+	ctx, cancel := context.WithTimeout(context.Background(), 2*time.Second)
+	defer cancel()
+
+	b := &cluster.ClientBundle{Dynamic: dynClient}
+	ch, err := Watch(ctx, b, podRef(), "default")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	go func() {
+		time.Sleep(10 * time.Millisecond)
+		fw.Add(newFakePod("new-pod", "default", "200"))
+		time.Sleep(10 * time.Millisecond)
+		fw.Delete(newFakePod("new-pod", "default", "201"))
+	}()
+
+	var types []string
+	for i := 0; i < 2; i++ {
+		select {
+		case e := <-ch:
+			types = append(types, e.Type)
+		case <-ctx.Done():
+			t.Fatalf("timed out after receiving %v", types)
+		}
+	}
+	if types[0] != "ADDED" || types[1] != "DELETED" {
+		t.Errorf("want [ADDED DELETED], got %v", types)
+	}
+}
